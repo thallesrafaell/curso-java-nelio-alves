bbf194bd15e5f6c4bf36f73ad758bf4e300c651d
Thalles Rafael <thallesrafaell@outlook.com>
2024-01-30 20:32:24 -0300

    jdbc4-atualizar-dados

---
+++ jdbc4-atualizar-dados/src/application/Program.java
@@ -0,0 +1,35 @@
+package application;
+
+import db.DB;
+
+
+import java.sql.*;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+
+public class Program {
+    public static void main(String[] args) {
+        Connection conn = null;
+        PreparedStatement st = null;
+
+        try {
+            conn = DB.getConnection();
+            st = conn.prepareStatement("UPDATE seller "
+            + "SET BaseSalary = BaseSalary + ? "
+            + "WHERE "
+            + "(DepartmentID = ?)");
+
+            st.setDouble(1, 200.0);
+            st.setInt(2, 4);
+
+            int rowsAffected = st.executeUpdate();
+            System.out.println("Done! Rows affected: " + rowsAffected);        }
+        catch (SQLException e){
+            e.printStackTrace();
+        }
+        finally {
+            DB.closeStatement(st);
+            DB.closeConnection();
+        }
+    }
+}
