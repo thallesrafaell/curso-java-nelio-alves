36239315b458e154a6d0b19d8139405a552e4e58
Thalles Rafael <thallesrafaell@outlook.com>
2024-01-30 20:32:51 -0300

    jdbc5-deletar-dados

---
+++ jdbc5-deletar-dados/src/application/Program.java
@@ -0,0 +1,35 @@
+package application;
+
+import db.DB;
+import db.DbIntegrityException;
+
+
+import java.sql.*;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+
+public class Program {
+    public static void main(String[] args) {
+        Connection conn = null;
+        PreparedStatement st = null;
+
+        try {
+            conn = DB.getConnection();
+            st = conn.prepareStatement("DELETE FROM seller "
+            + "WHERE "
+            + "Id = ?"
+            );
+
+            st.setInt(1, 8);
+
+            int rowsAffected = st.executeUpdate();
+            System.out.println("Done! Rows affected: " + rowsAffected);        }
+        catch (SQLException e){
+            throw  new DbIntegrityException(e.getMessage());
+        }
+        finally {
+            DB.closeStatement(st);
+            DB.closeConnection();
+        }
+    }
+}
